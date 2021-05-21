ef620ec040c70e1e3d6389febd3e2d20274a0e65
Chase Abbott <cabbott93@gmail.com>
2021-05-21 16:52:11 -0700

    scripts and stuff

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const dotenv = require('dotenv');
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -18,6 +19,8 @@ io.on('connection', (socket) => {
   })
 });
 
-server.listen(3000, () => {
+const port = process.env.port || 3000
+
+server.listen(port, () => {
   console.log('listening on *:3000');
 });
