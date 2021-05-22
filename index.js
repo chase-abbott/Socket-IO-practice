760739241b8dd36d0c1d66cb0b299e9b8d05f723
Chase Abbott <cabbott93@gmail.com>
2021-05-21 17:34:29 -0700

    heroku changes

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const socketIO = require('socket.io');
+// const socketIO = require('socket.io');
 
-const INDEX = '/index.html';
+// const INDEX = '/index.html';
 const PORT = process.env.port || 3000;
 
 dotenv.config();
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-const io = socketIO(server)
+const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  })
-});
+server.get('/', (req, res) => {
+  res.send('Welcome to the socket io')
+})
+
+// const io = socketIO(server)
+
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+//   socket.on('chat message', (msg) => {
+//     io.emit('chat message', msg);
+//   })
+// });
