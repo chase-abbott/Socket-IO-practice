ca91e4309c13a010e56f20c43bd6d7de85ffc664
Gabriel Simek <gabrielsimek@outlook.com>
2021-05-26 14:44:16 -0700

    for heroku

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const { Server } = require('socket.io');
 // const { disconnect } = require('process');
 const io = new Server(server, {
   cors: {
-    // origin: 'https://mystifying-bardeen-9951c5.netlify.app',
-    origin: 'http://localhost:3001',
+    origin: 'https://mystifying-bardeen-9951c5.netlify.app',
+    // origin: 'http://localhost:3001',
     methods: ['GET', 'POST']                                                       
   }
 });
