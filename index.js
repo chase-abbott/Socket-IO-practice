6fe75dacaa6963670b1b07607ebaada8f9520b56
Gabriel Simek <gabrielsimek@outlook.com>
2021-05-26 09:42:19 -0700

    localhost

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
     // origin: 'https://mystifying-bardeen-9951c5.netlify.app/draft',
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST']                                                       
   }
 });
